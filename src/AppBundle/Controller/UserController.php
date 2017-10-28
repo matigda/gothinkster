a0167dd7fb8d46c4c1c856ed64d099acab9b8ef2
Tomasz Wesolowski <wesol05@gmail.com>
2017-10-28 16:10:01 +0200

    simplify use case service definition

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -4,10 +4,14 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
 use AppBundle\UseCase\Command\GetUserProfileCommand;
+use AppBundle\UseCase\FollowUser;
+use AppBundle\UseCase\GetUserProfileUseCase;
 use AppBundle\UseCase\RegisterUser;
 use AppBundle\UseCase\RegisterUserCommand;
 use AppBundle\UseCase\FollowUserCommand;
+use AppBundle\UseCase\UnfollowUser;
 use AppBundle\UseCase\UnfollowUserCommand;
+use Core\Repository\UserRepositoryInterface;
 use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations\Delete;
 use FOS\RestBundle\Controller\Annotations as Rest;
@@ -55,7 +59,7 @@ class UserController extends Controller
     public function profileAction(string $username)
     {
         return [
-            'profile' => $this->get('use_case.get_user_profile')->execute(
+            'profile' => $this->get(GetUserProfileUseCase::class)->execute(
                 new GetUserProfileCommand($username, $this->getUser())
             )
         ];
@@ -66,26 +70,26 @@ class UserController extends Controller
      */
     public function followUserAction(string $username)
     {
-        $userToFollow = $this->getDoctrine()->getRepository(User::class)->findOneBy(compact('username'));
-        $this->get('use_case.user_follow_user')->execute(new FollowUserCommand($this->getUser(), $userToFollow));
+        $userToFollow = $this->get(UserRepositoryInterface::class)->findOneBy(compact('username'));
+        $this->get(FollowUser::class)->execute(new FollowUserCommand($this->getUser(), $userToFollow));
 
         return [
-            'profile' => $this->get('use_case.get_user_profile')->execute(
+            'profile' => $this->get(GetUserProfileUseCase::class)->execute(
                 new GetUserProfileCommand($username, $this->getUser())
             )
         ];
     }
 
     /**
-     * @Delete("/profiles/{username}/follow")
+     * @Delete("/profiles/{username}/follow")UserRe
      */
     public function unfollowUserAction(string $username)
     {
-        $userToUnfollow = $this->getDoctrine()->getRepository(User::class)->findOneBy(compact('username'));
-        $this->get('use_case.user_unfollow_user')->execute(new UnfollowUserCommand($this->getUser(), $userToUnfollow));
+        $userToUnfollow = $this->get(UserRepositoryInterface::class)->findOneBy(compact('username'));
+        $this->get(UnfollowUser::class)->execute(new UnfollowUserCommand($this->getUser(), $userToUnfollow));
 
         return [
-            'profile' => $this->get('use_case.get_user_profile')->execute(
+            'profile' => $this->get(GetUserProfileUseCase::class)->execute(
                 new GetUserProfileCommand($username, $this->getUser())
             )
         ];
